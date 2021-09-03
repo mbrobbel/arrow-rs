35a7143a91d29d7082860d22f2b55b39eb619305
Matthijs Brobbel <m1brobbel@gmail.com>
2021-09-03 13:13:02 +0200

    Fix build on ppc64le

--- parquet/src/util/hash_util.rs
+++ parquet/src/util/hash_util.rs
@@ -33,7 +33,11 @@ fn hash_(data: &[u8], seed: u32) -> u32 {
         }
     }
 
-    #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+    #[cfg(any(
+        target_arch = "aarch64",
+        target_arch = "arm",
+        target_arch = "powerpc64"
+    ))]
     unsafe {
         murmur_hash2_64a(data, seed as u64) as u32
     }
